85d1d64b9f5a87914549156d00107f6c630d4bc3
Kristina Devochko <guidemetothemoon@gmail.com>
2023-09-15 11:00:08 +0200

    fix: clean up unnecessary operation check logic for import
    Signed-off-by: Kristina Devochko <guidemetothemoon@gmail.com>

--- azure/keyvault/Import-Certificate-To-Keyvault.ps1
+++ azure/keyvault/Import-Certificate-To-Keyvault.ps1
@@ -10,26 +10,7 @@ param(
 
     $certificatePassword = ConvertTo-SecureString -String $certificatePwd -AsPlainText -Force
     Import-AzKeyVaultCertificate -VaultName $vaultName -Name $certificateName -CertificateString $certificateData -Password $certificatePassword
+    
     $newCert = Get-AzKeyVaultCertificate -VaultName $vaultName -Name $certificateName
-
-    # Wait until upload to Key Vault is finished
-    $tries = 0
-    do {
-    Write-Host 'Waiting for certificate import completion...'
-    Start-Sleep -Seconds 10
-    $operation = Get-AzKeyVaultCertificateOperation -VaultName $vaultName -Name $certificateName
-    $tries++
-
-    if ($operation.Status -eq 'failed')
-    {
-        throw 'Importing certificate $certificateName in vault $vaultName failed with error $($operation.ErrorMessage)'
-    }
-
-    if ($tries -gt 120)
-    {
-        throw 'Timed out waiting for import of certificate $certificateName in vault $vaultName'
-    }
-    } while ($operation.Status -ne 'completed')
-
     $DeploymentScriptOutputs['certThumbprint'] = $newCert.Thumbprint
     $newCert | Out-String
