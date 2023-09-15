4734efcc2d3adeed068492794a861d7cdac9e24c
Kristina Devochko <guidemetothemoon@gmail.com>
2023-09-15 09:25:04 +0200

    feat: :sparkles: add import cert to keyvault script
    Signed-off-by: Kristina Devochko <guidemetothemoon@gmail.com>

---
+++ azure/keyvault/Import-Certificate-To-Keyvault.ps1
@@ -0,0 +1,34 @@
+param(
+    [string] [Parameter(Mandatory=$true)] $vaultName,    
+    [string] [Parameter(Mandatory=$true)] $certificateData,
+    [string] [Parameter(Mandatory=$true)] $certificatePwd,
+    [string] [Parameter(Mandatory=$false)] $certificateName = 'appgwcert'
+)
+
+    $ErrorActionPreference = 'Stop'
+    $DeploymentScriptOutputs = @{}
+    
+    Import-AzKeyVaultCertificate -VaultName $vaultName -Name $certificateName -CertificateString $certificateData -Password $certificatePwd
+    $newCert = Get-AzKeyVaultCertificate -VaultName $vaultName -Name $certificateName
+
+    # Wait until upload to Key Vault is finished
+    $tries = 0
+    do {
+    Write-Host 'Waiting for certificate import completion...'
+    Start-Sleep -Seconds 10
+    $operation = Get-AzKeyVaultCertificateOperation -VaultName $vaultName -Name $certificateName
+    $tries++
+
+    if ($operation.Status -eq 'failed')
+    {
+        throw 'Importing certificate $certificateName in vault $vaultName failed with error $($operation.ErrorMessage)'
+    }
+
+    if ($tries -gt 120)
+    {
+        throw 'Timed out waiting for import of certificate $certificateName in vault $vaultName'
+    }
+    } while ($operation.Status -ne 'completed')
+
+    $DeploymentScriptOutputs['certThumbprint'] = $newCert.Thumbprint
+    $newCert | Out-String
