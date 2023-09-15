5b9044b8840c77d8482227602cf0c17142598f59
Kristina Devochko <guidemetothemoon@gmail.com>
2023-09-15 10:32:29 +0200

    fix: make certificate name mandatory
    Signed-off-by: Kristina Devochko <guidemetothemoon@gmail.com>

--- azure/keyvault/Import-Certificate-To-Keyvault.ps1
+++ azure/keyvault/Import-Certificate-To-Keyvault.ps1
@@ -2,12 +2,12 @@ param(
     [string] [Parameter(Mandatory=$true)] $vaultName,    
     [string] [Parameter(Mandatory=$true)] $certificateData,
     [string] [Parameter(Mandatory=$true)] $certificatePwd,
-    [string] [Parameter(Mandatory=$false)] $certificateName = 'appgwcert'
+    [string] [Parameter(Mandatory=$true)] $certificateName
 )
 
     $ErrorActionPreference = 'Stop'
     $DeploymentScriptOutputs = @{}
-    
+
     $certificatePassword = ConvertTo-SecureString -String $certificatePwd -AsPlainText -Force
     Import-AzKeyVaultCertificate -VaultName $vaultName -Name $certificateName -CertificateString $certificateData -Password $certificatePassword
     $newCert = Get-AzKeyVaultCertificate -VaultName $vaultName -Name $certificateName
