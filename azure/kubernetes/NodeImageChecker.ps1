d290e493da46d887d25ca0f7e5cfa8bbfcc2e6a0
Roberth Strand <roberth.strand@amesto.no>
2023-10-11 13:23:56 +0200

    feat: various improvements
    Added documentation, a better multi-tenant and multi-subscription processing, and return the result as an object.
    
    Signed-off-by: Roberth Strand <roberth.strand@amesto.no>

--- azure/kubernetes/NodeImageChecker.ps1
+++ azure/kubernetes/NodeImageChecker.ps1
@@ -1,34 +1,92 @@
+<#
+.SYNOPSIS
+    This script retrieves the node image versions for all AKS clusters in a subscription or across all subscriptions.
+
+.PARAMETER SubscriptionId
+    The ID of the subscription to retrieve AKS clusters from.
+
+.PARAMETER TenantId
+    The ID of the tenant to retrieve AKS clusters from.
+
+.PARAMETER Name
+    The name of the AKS cluster to retrieve.
+
+.PARAMETER ResourceGroupName
+    The name of the resource group containing the AKS cluster to retrieve.
+
+.PARAMETER AllSubscriptions
+    If specified, retrieves AKS clusters from all subscriptions.
+
+.OUTPUTS
+    Returns an array of objects containing the name of the AKS cluster, the subscription it belongs to, and an array of node image versions.
+
+.EXAMPLE
+    PS C:\> .\NodeImageChecker.ps1 -SubscriptionId "12345678-1234-1234-1234-123456789012"
+
+    Retrieves the node image versions for all AKS clusters in the specified subscription.
+
+.EXAMPLE
+    PS C:\> .\NodeImageChecker.ps1 -AllSubscriptions
+
+    Retrieves the node image versions for all AKS clusters across all subscriptions.
+
+#>
+
 [CmdletBinding()]
 param (
     [Parameter()]
     [String]
-    $TenantId
+    $SubscriptionId,
+    [Parameter()]
+    [String]
+    $TenantId,
+    [Parameter()]
+    [String]
+    $Name,
+    [Parameter()]
+    [String]
+    $ResourceGroupName,
+    [Parameter()]
+    [Switch]
+    $AllSubscriptions
 )
 
 $NodeImageVersions = @()
 $Clusters = @()
 
-if (!$TenantId) {
-    $Clusters = Get-AzAksCluster
-} else {
-    $AllSubscriptions = Get-AzSubscription -TenantId $TenantId | Where-Object { $_.State -eq 'Enabled' }
+if ($TenantId) {
+    $Subscriptions = Get-AzSubscription -TenantId $TenantId | Where-Object { $_.State -eq 'Enabled' }
     $i = 0
-    foreach ($Subscription in $AllSubscriptions) {
+    foreach ($Subscription in $Subscriptions) {
         $i++
-        $Completed = ($i/$AllSubscriptions.Count)*100
-        Write-Progress -Activity "Finding all Kubernetes clusters" -Status "Progress:" -PercentComplete $Completed
+        $Completed = ($i/$Subscriptions.Count)*100
+        Write-Progress -Activity "Finding all Kubernetes clusters, subscription $i of $($Subscriptions.count)" -Status "Progress:" -PercentComplete $Completed
         $Clusters += Get-AzAksCluster -Subscription $Subscription.Id
     }
+} elseif ($SubscriptionId) {
+    $Clusters += Get-AzAksCluster -Subscription $SubscriptionId
+} elseif ($ClusterName) {
+    $Clusters += Get-AzAksCluster -Name $ClusterName
+} elseif ($AllSubscriptions) {
+    $Subscriptions = Get-AzSubscription | Where-Object { $_.State -eq 'Enabled' }
+    $i = 0
+    foreach ($Subscription in $Subscriptions) {
+        $i++
+        $Completed = ($i/$Subscriptions.Count)*100
+        Write-Progress -Activity "Finding all Kubernetes clusters, subscription $i of $($Subscriptions.count)" -Status "Progress:" -PercentComplete $Completed
+        $Clusters += Get-AzAksCluster -Subscription $Subscription.Id
+    }
+} else {
+    $Clusters += Get-AzAksCluster
 }
 
-$Clusters | ForEach-Object -Begin {
-    Write-Host "Checking node image versions for $($Clusters.Count) clusters..."
-} -Process {
+foreach ($Cluster in $Clusters) {
     $ImageVersions = $Cluster.AgentPoolProfiles.NodeImageVersion | Sort-Object -Unique
     $NodeImageVersions += [PSCustomObject]@{
         ClusterName = $Cluster.Name
+        Subscription = $Cluster.Id.Split('/')[2]
         NodeImageVersions = $ImageVersions
     }
 }
 
-$NodeImageVersions | Format-Table -AutoSize
+return $NodeImageVersions
