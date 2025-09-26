7f65c4e9705cf6ba1b050216fbfacf1f714405b1
Tim Peter Edstrøm <tim.peter.edstrom@fortytwo.io>
2025-09-26 08:26:47 +0200

    fix: add $WorkingDirectory, fix $runSettings, add comments/region, example
    Signed-off-by: Tim Peter Edstrøm <tim.peter.edstrom@fortytwo.io>

--- checkid/password-agent/Register-ScheduledTasks.ps1
+++ checkid/password-agent/Register-ScheduledTasks.ps1
@@ -17,41 +17,75 @@
 
 .EXAMPLE
     PS C:\> .\Register-ScheduledTasks.ps1
+
+.EXAMPLE
+    PS C:\> .\Register-ScheduledTasks.ps1 -script c:\checkid\run.ps1 -gMSA account$ -runTask "Start agent" -taskStart "05:01" -preTask "Stop agent" -taskStop "05:00"
 #>
 
 param(
-    [string] $pwsh = 'C:\Program Files\PowerShell\7\pwsh.exe',
+    # PowerShell 7+ executable, including full path
+    [string] $pwsh = '"C:\Program Files\PowerShell\7\pwsh.exe"',
+    # Script including full path
     [string] $script = 'C:\checkid\StartStop-CheckIDAgentListener.ps1',
-    [string] $gMSA = 'checkidgMSA',
+    # Account name can be, and not limited to, group managed service account (if using a gMSA, including a trailing $)
+    [string] $gMSA = 'checkidgMSA$',
+    # Name of scheduled task that will run the password change agent. To avoid multiple instances the `$preTask will be run just before to stop the current task
     [string] $runTask = 'CheckID - start password change agent',
-    [string] $preTask = 'CheckID - stop agent for daily restart',
+    # Time of day that `$runTask will start
     [String] $taskStart = "03:04",
+    # Name of scheduled task that will stop the password change agent configured in `$runTask for a daily restart
+    [string] $preTask = 'CheckID - stop agent for daily restart',
+    # Time of day that `$preTask will start
     [string] $taskStop = "03:03",
+    # Time (in seconds) that `$preTask waits before executing (Start-Sleep)
     [int] $waitBeforeStopInSeconds = 35,
+    # Time (in seconds) that `$preTask will allow to pass, after trying to stop `$runTask, before throwing an error that `$runTask could not be stopped
     [int] $gracePeriodInSeconds = 15
 )
 
-# RUN: At startup + daily at $taskStart
-$runAction = New-ScheduledTaskAction -Execute $pwsh -Argument "-NoLogo -NoProfile -ExecutionPolicy Bypass -File `"$script`""
+#region RUN: At startup + daily at $taskStart
+
+# Calculate working directory from `$script variable
+$scriptName = $script.Split('\')[-1]
+$workingDir = ($script -replace $scriptName, '').Trim('\')
+
+$runAction = New-ScheduledTaskAction -Execute $pwsh -WorkingDirectory `"$workingDir`" -Argument "-NoLogo -NoProfile -ExecutionPolicy Bypass -File `"$script`""
+
 # OPTIONAL - PowerShell cmdlet New-ScheduledTaskTrigger helps to convert -At parameter value into [DateTime] object if it senses the input to be of type string
 $taskStartDatetime = [datetime]::ParseExact($taskStart, "HH:mm", $null)
+
+# Configure triggers for when to start `$runTask
 $runTriggers = @(
     (New-ScheduledTaskTrigger -AtStartup),
     (New-ScheduledTaskTrigger -Daily -At $taskStartDateTime)
 )
-$runSettings = New-ScheduledTaskSettingsSet -RestartCount 5 -RestartInterval (New-TimeSpan -Minutes 1) -ExecutionTimeLimit (New-TimeSpan -Seconds 0) -StartWhenAvailable $runSettings.CimInstanceProperties.Item('MultipleInstances').Value = 3   # 3 corresponds to 'Stop the existing instance'
-    
+# Configure scheduled task settings which is applied when the task is running 
+$runSettings = New-ScheduledTaskSettingsSet -RestartCount 5 -RestartInterval (New-TimeSpan -Minutes 1) -ExecutionTimeLimit (New-TimeSpan -Seconds 0) -StartWhenAvailable
+# Set property with value 3. Corresponds to 'Stop the existing instance'
+$runSettings.CimInstanceProperties.Item('MultipleInstances').Value = 3 
+
+# Provide identity in which context the scheduled task will run and set to run with highest (admin) RunLevel, if the identity holds such privileges
 $principal = New-ScheduledTaskPrincipal -UserId $gMSA -LogonType Password -RunLevel Highest
-    
+
+# Look for an existing scheduled task with the name 
 if (Get-ScheduledTaskInfo -TaskName $runTask -ErrorAction SilentlyContinue) { Unregister-ScheduledTask -TaskName $runTask -Confirm:$false -ErrorAction Stop }
-Register-ScheduledTask -TaskName $runTask -Action $runAction -Trigger $runTriggers -Settings $runSettings -Principal $principal -Description "Triggers $($script.Split('\')[-1]) which runs continously. Script polls Fortytwo API for pending password reset requests from CheckID onboarding flow."
+# Register scheduled task
+Register-ScheduledTask -TaskName $runTask -Action $runAction -Trigger $runTriggers -Settings $runSettings -Principal $principal -Description "Triggers $scriptName which runs continously. Script polls Fortytwo API for pending password reset requests from CheckID onboarding flow."
+# Start scheduled task
 Start-ScheduledTask -TaskName $runTask
-    
-# STOP: daily at $taskStop using -StopTask with -WaitToStopTaskInSeconds and -GraceSeconds
-$preAction = New-ScheduledTaskAction -Execute $pwsh -Argument "-NoLogo -NoProfile -ExecutionPolicy Bypass -File `"$script`" -StopTask -TaskName `"$runTask`" -WaitToStopTaskInSeconds $waitBeforeStopInSeconds -GraceSeconds $gracePeriodInSeconds"
+#endregion
+
+#region STOP: daily at $taskStop using -StopTask with -WaitToStopTaskInSeconds and -GraceSeconds
+$preAction = New-ScheduledTaskAction -Execute $pwsh -WorkingDirectory `"$workingDir`" -Argument "-NoLogo -NoProfile -ExecutionPolicy Bypass -File `"$script`" -StopTask -TaskName `"$runTask`" -WaitToStopTaskInSeconds $waitBeforeStopInSeconds -GraceSeconds $gracePeriodInSeconds"
+
 # OPTIONAL - PowerShell cmdlet New-ScheduledTaskTrigger helps to convert -At parameter value into [DateTime] object if it senses the input to be of type string
 $taskStopDatetime = [datetime]::ParseExact($taskStop, "HH:mm", $null)
+
 $preTrigger = New-ScheduledTaskTrigger -Daily -At $taskStopDatetime
 $preSettings = New-ScheduledTaskSettingsSet -StartWhenAvailable
+
+# Look for an existing scheduled task with the name 
 if (Get-ScheduledTaskInfo -TaskName $preTask -ErrorAction SilentlyContinue) { Unregister-ScheduledTask -TaskName $preTask -Confirm:$false -ErrorAction Stop }
-Register-ScheduledTask -TaskName $preTask -Action $preAction -Trigger $preTrigger -Settings $preSettings -Principal $principal -Description "Stops $($script.Split('\')[-1]) before daily restart."
+# Register scheduled task
+Register-ScheduledTask -TaskName $preTask -Action $preAction -Trigger $preTrigger -Settings $preSettings -Principal $principal -Description "Stops $scriptName before daily restart."
+#endregion
